7aea2178900ccc4f7dd1b20563b5d9821d80abb7
Calum <57592605+calum-s@users.noreply.github.com>
2022-10-27 17:45:23 +0100

    init

---
+++ components/Head.js
@@ -0,0 +1,9 @@
+export default function(){
+    return (
+        <Head>
+        <title>M23-GC</title>
+        <meta name="Mach23 Ground Control" content="Mach23 Ground Control website for StrathSEDS" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+    )
+}
